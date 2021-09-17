d6446d0eac927848bf670310152024bebbc4f0d3
jdfwsp <joshfergusonsp@gmail.com>
2021-09-17 09:42:41 -0500

    Updates Crowsale Constructors

--- Code/Crowdsale.sol
+++ Code/Crowdsale.sol
@@ -18,9 +18,16 @@ contract PupperCoinSale is Crowdsale,
         // @TODO: Fill in the constructor parameters!
         uint rate,
         address payable wallet,
-        PupperCoin token
+        PupperCoin token,
+        uint goal,
+        uint openingTime,
+        uint closingTime
     )
         // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        Crowdsale(rate, wallet, token)
+        TimedCrowdsale(openingTime, closingTime)
+        RefundableCrowdsale(goal)
+        
         public
     {
         // constructor can stay empty
@@ -34,12 +41,22 @@ contract PupperCoinSaleDeployer {
 
     constructor(
         // @TODO: Fill in the constructor parameters!
+        string memory name,
+        string memory symbol,
+        address payable wallet
     )
         public
     {
         // @TODO: create the PupperCoin and keep its address handy
-
+		PupperCoin token = new PupperCoin(name, symbol, 0);
+		token_address = address(token);
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        PupperCoinSale pupper_sale = new PupperCoinSale(1, wallet, token);
+        pupper_sale_address = address(pupper_sale);
+        goal = 300;
+        openingTime = now;
+        closingTime = now + 24 weeks;
+        
 
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
